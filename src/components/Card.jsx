f7dc6cd9e1fb3111a2ebb5f42c40ee243780ca9b
Hardik1703 <gambhavahardik1703@gmail.com>
2023-12-23 00:28:16 +0530

    First Commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <>
+        <div className='w-60 h-72 rounded-[40px] bg-sky-400 rou'></div>
+    </>
+  )
+}
+
+export default Card
---
+++ src/components/Foreground.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './Card'
+
+const Foreground = () => {
+    return (
+        <>
+            <div className='fixed z-[3] top-0 left-0 w-full h-full'></div>
+            <Card />
+        </>
+    )
+}
+
+export default Foreground
