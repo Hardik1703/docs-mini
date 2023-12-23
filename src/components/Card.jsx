a8aad4beb48ff5e38b703d720527ee9864abff50
Hardik1703 <gambhavahardik1703@gmail.com>
2023-12-23 12:28:00 +0530

    Card Details Dynamic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { FaRegFileAlt } from "react-icons/fa";
+import { GoDownload } from "react-icons/go";
+import { IoClose } from "react-icons/io5";
 
-function Card() {
+function Card({ data }) {
   return (
     <>
-        <div className='w-60 h-72 rounded-[40px] bg-sky-400 rou'></div>
+      <div className='relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-900/90 px-5 py-10 text-white overflow-hidden'>
+        <FaRegFileAlt />
+        <p className='text-sm font-semibold mt-5 leading-tight'>{data.desc}</p>
+        <div className='footer absolute bottom-0 left-0 w-full'>
+          <div className='flex items-center justify-between mb-3 py-3 px-8'>
+            <h5>{data.filesize}</h5>
+            <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
+              {data.close ? <IoClose /> : <GoDownload size=".9em" color="#fff" />}
+            </span>
+          </div>
+          {data.tag.isopen && (
+            <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
+              <h3 className='text-sm font-semibold'>Download Now</h3>
+            </div>
+          )}
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -2,10 +2,33 @@ import React from 'react'
 import Card from './Card'
 
 const Foreground = () => {
+    const data = [
+        {
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+            filesize: ".9mb",
+            close: true,
+            tag: { isopen: true, tagTitle: "Download Now" },
+        },
+        {
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing dolor sit amet.",
+            filesize: ".1mb",
+            close: false,
+            tag: { isopen: false, tagTitle: "Download Now" },
+        },
+        {
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+            filesize: ".9mb",
+            close: true,
+            tag: { isopen: true, tagTitle: "Download Now" },
+        },
+    ];
     return (
         <>
-            <div className='fixed z-[3] top-0 left-0 w-full h-full'></div>
-            <Card />
+            <div className='fixed z-[3] top-0 left-0 w-full h-full flex flex-wrap gap-5'>
+                {data.map((items, index) => (
+                    <Card data={items} />
+                ))}
+            </div>
         </>
     )
 }
